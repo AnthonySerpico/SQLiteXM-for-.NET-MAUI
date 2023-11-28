e9efeb43ec30101698597fc42aa9ac22316bf065
Anthony Serpico <ajser@LAPTOP-IJC0FLM3>
2023-11-28 10:02:16 -0500

    Renamed insert definitions file to sql definitions and rolled the alter definition into the sql definition file.

--- SQLiteXMCL/SqlDefinitions.cs
+++ SQLiteXMCL/SqlDefinitions.cs
@@ -119,5 +119,25 @@
             this.indexSQL = indexSQL;
         }
     }
+
+    public class AlterDefinition
+    {
+        private string columnName;
+        public string ColumnName
+        {
+            get { return columnName; }
+        }
+        private string alterSQL;
+        public string AlterSQL
+        {
+            get { return alterSQL; }
+        }
+
+        internal AlterDefinition(string columnName, string alterSQL)
+        {
+            this.columnName = columnName;
+            this.alterSQL = alterSQL;
+        }
+    }
 }
 
